97e2328b6915728997f6fdf23d4a569914b3008c
maxw <maxime.wacker@free.fr>
2021-04-01 14:47:39 +0200

    Introducing urgent mark on master cell (but disabled because bugy)

--- AppScenes/ProductListScene/Sources/UI/ProductListCell.swift
+++ AppScenes/ProductListScene/Sources/UI/ProductListCell.swift
@@ -23,13 +23,32 @@ final class ProductListCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        productImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
         
+        contentView.addSubview(topHStack)
+        topHStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        topHStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        topHStack.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        topHStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+        productImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        productImageView.layer.cornerRadius = 4.0
+
         productImageView.contentMode = .scaleAspectFit
         productImageView.clipsToBounds = true
+        
+        textVStack.addSubview(self.urgentMark)
+        urgentMark.trailingAnchor.constraint(equalTo: self.textVStack.trailingAnchor).isActive = true
+        textVStack.superview?.bringSubviewToFront(self.urgentMark)
 
-        let textVStack = UIStackView(
-            arrangedSubviews: [titleLabel, priceLabel])
+        DispatchQueue.main.async  {
+            self.showImageLoading()
+        }
+        
+    }
+        
+    private lazy var textVStack: UIStackView = {
+        let textVStack = UIStackView( arrangedSubviews: [titleLabel, priceLabel])
+        textVStack.layer.cornerRadius = 4
         textVStack.backgroundColor = .lightGray
         textVStack.translatesAutoresizingMaskIntoConstraints = false
         textVStack.axis = .vertical
@@ -37,9 +56,11 @@ final class ProductListCell: UITableViewCell {
         textVStack.spacing = UIStackView.spacingUseSystem
         textVStack.isLayoutMarginsRelativeArrangement = true
         textVStack.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        return textVStack
+    }()
         
         
-        
+    private lazy var topHStack: UIStackView = {
         let topHStack = UIStackView(arrangedSubviews: [productImageView, textVStack])
         topHStack.translatesAutoresizingMaskIntoConstraints = false
         topHStack.axis = .horizontal
@@ -49,17 +70,9 @@ final class ProductListCell: UITableViewCell {
         topHStack.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
         contentView.layoutMargins.right = 5
         
-        contentView.addSubview(topHStack)
-        
-        topHStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-        topHStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-        topHStack.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        topHStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-        
-        DispatchQueue.main.async  {
-            self.showImageLoading()
-        }
-    }
+        return topHStack
+    } ()
+
     
     func showImageLoading(style: UIActivityIndicatorView.Style = .gray){
         let loading = (viewWithTag(ProductListCell.loadingViewTag) as? UIActivityIndicatorView) ?? UIActivityIndicatorView(style: style)
@@ -78,14 +91,6 @@ final class ProductListCell: UITableViewCell {
         loading?.removeFromSuperview()
     }
     
-    
-    var productItem: ProductCellMainViewModel? {
-        didSet {
-            titleLabel.text = productItem?.title
-            priceLabel.text = productItem?.price
-        }
-    }
-    
     private let titleLabel : UILabel = {
         let lbl = UILabel()
         lbl.setContentHuggingPriority(UILayoutPriority(rawValue: 0), for: .horizontal)
@@ -98,6 +103,7 @@ final class ProductListCell: UITableViewCell {
     
     private let priceLabel : UILabel = {
         let lbl = UILabel()
+        lbl.setContentHuggingPriority(UILayoutPriority(rawValue: 0), for: .horizontal)
         lbl.textColor = .black
         lbl.font = UIFont.systemFont(ofSize: 16)
         lbl.textAlignment = .left
@@ -105,11 +111,22 @@ final class ProductListCell: UITableViewCell {
         return lbl
     }()
     
+    
+    var urgentMark : UIImageView = {
+        let urgentMark = UIImageView()
+        urgentMark.image = UIImage(named: "urgent")
+        urgentMark.translatesAutoresizingMaskIntoConstraints = false
+        urgentMark.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        urgentMark.contentMode = .scaleAspectFit
+        urgentMark.clipsToBounds = true
+        return urgentMark
+    }()
+    
     override func prepareForReuse() {
         super.prepareForReuse()
+        presenter = nil
         titleLabel.text = nil
         priceLabel.text = nil
-        productImageView.image = nil
         DispatchQueue.main.async  {
             self.showImageLoading()
         }
@@ -128,9 +145,18 @@ extension ProductListCell: ProductListCelling{
     
     func updateMain(_ mainViewModel: ProductCellMainViewModel) {
         DispatchQueue.main.async  {
-            self.productItem = mainViewModel
+            self.titleLabel.text = mainViewModel.title
+            self.priceLabel.text = mainViewModel.price
+            //print("cell \(mainViewModel.title) Urgent: \(mainViewModel.isUrgent) ")
+            self.urgentMark.image = nil //disabled because bugy
+// Horrible bug to be fixed
+// When uncommenting next 2 lines, the urgent mark doesn't appear on the same cells as the one
+// colored purple but on the cell next to it !!!
+
+//            self.textVStack.backgroundColor = mainViewModel.isUrgent ? UIColor.purple : UIColor.lightGray
+//            self.urgentMark.image = mainViewModel.isUrgent ? UIImage(named: "urgent") : nil
+            self.layoutSubviews()
         }
     }
-    
     
 }
